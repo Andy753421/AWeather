f7cd7dd42c160efc116f4597782c8f7d11f7c2ca
Andy Spencer <andy753421@gmail.com>
2009-10-24 18:53:45 +0000

    Adding -Wl,-z,defs to gen.sh

--- gen.sh
+++ gen.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 dir=$(dirname $(readlink -f $0))
-CFLAGS="-g -Werror -Wno-unused $CFLAGS" \
 ./autogen.sh \
 	"--libdir=$dir/src/plugins" \
 	"--datadir=$dir/data" \
-	--enable-gtk-doc
+	--enable-gtk-doc \
+	CFLAGS="-g -Werror -Wno-unused $CFLAGS" \
+	LDFLAGS="-Wl,-z,defs"
