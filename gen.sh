459a68347c21b94a7017e36d551ac78ee74b51c9
Andy Spencer <andy753421@gmail.com>
2011-09-09 06:22:30 +0000

    Remove no-undefined-symbols from gen.sh
    This should be done properly in the Makefiles now.

--- gen.sh
+++ gen.sh
@@ -4,5 +4,4 @@ PKG_CONFIG_PATH="../grits/src/" \
 ./autogen.sh \
 	"--datadir=$dir/data" \
 	"--libdir=$dir/src/plugins" \
-	CFLAGS="-g -Werror $CFLAGS" \
-	LDFLAGS="-Wl,-z,defs"
+	CFLAGS="-g -Werror $CFLAGS"
